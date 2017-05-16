f96025afb5e6cd14a0443fbf6130339f996d81ab
AndreyK <andrey.kozhevnikov@devexpress.com>
2017-05-16 13:05:08 +0300

    Add project files.

---
+++ MyMusic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic {
+    class Program {
+        static void Main(string[] args) {
+            var m = new MusicProcessor();
+            m.CollectMusic();
+        }
+    }
+
+    public class MusicProcessor {
+        string folderPath = @"c:\MyMusic\";
+        public void CollectMusic() {
+            var lst = Directory.GetFiles(folderPath,"*.mp3*",SearchOption.AllDirectories);
+            var lstSt = string.Join(Environment.NewLine, lst);
+            using(var sw=new StreamWriter(@"c:\dropbox\MyMusic.txt")) {
+                sw.Write(lstSt);
+                sw.Close();
+            }
+        }
+    }
+}
